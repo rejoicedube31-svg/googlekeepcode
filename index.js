7f41b127cd90154dc5b1b194b17cceed135ae359
rejoice dube <rejoicedube31@gmail.com>
2026-03-07 19:28:55 +0200

    variables declared

---
+++ index.js
@@ -0,0 +1,13 @@
+//two pieces of input are used to create a component in the page when a button is clicked.
+
+//Input Process Output process
+
+ function readInput () {
+    let title = document.getElementById("title")
+                        .value;
+
+    let note = document.getElementById("note")
+                        .value;
+
+
+ }
