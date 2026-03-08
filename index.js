1bb3edd407aa37ba27ee85955e41a57c4dbeb41a
rejoice dube <rejoicedube31@gmail.com>
2026-03-08 21:48:27 +0200

    js and html updated

--- index.js
+++ index.js
@@ -9,5 +9,28 @@
     let note = document.getElementById("note")
                         .value;
 
-
+    return [title, note];
  }
+
+    function createNote (title, note) {
+        let Container = document.createElement("div");
+        let titleElement = document.createElement("h2");
+        let noteElement = document.createElement("p");
+
+        titleElement.textContent = title;
+        noteElement.textContent = note;
+
+        Container.appendChild(titleElement);
+        Container.appendChild(noteElement);
+        Container.id = "note";
+
+        return Container;
+}
+
+function displayNote () {
+    let [title, note] = readInput();
+    let container = createNote(title, note);
+
+    let noteList = document.getElementById("note-list");
+    noteList.appendChild(container);
+}
